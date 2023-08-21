bead3d2c527af4883d345152d3b7e67bb0f0084a
Khoahello <dinhtankhoa2k7@gmail.com>
2023-08-21 23:54:41 +0700

    update hairsyle

---
+++ controllers/main.js
@@ -0,0 +1,198 @@
+// Top Cloth
+function thuTopCloth1() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth1.png" alt="" />
+    `
+}
+
+function thuTopCloth2() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth2.png" alt="" />
+    `
+}
+
+function thuTopCloth3() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth3.png" alt="" />
+    `
+}
+
+function thuTopCloth4() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth4.png" alt="" />
+    `
+}
+
+function thuTopCloth5() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth5.png" alt="" />
+    `
+}
+
+function thuTopCloth6() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth6.png" alt="" />
+    `
+}
+
+// Bot Cloth
+function thuBotCloth1() {
+    document.getElementById("bikinibottom").innerHTML = /*html*/ `
+        <img id="thuBotCloth" class="bikinibottom" src="../assets/images/clothes/botcloth1.png" alt="" />
+    `
+}
+
+function thuBotCloth2() {
+    document.getElementById("bikinibottom").innerHTML = /*html*/ `
+        <img id="thuBotCloth" class="bikinibottom" src="../assets/images/clothes/botcloth2.png" alt="" />
+    `
+}
+
+function thuBotCloth3() {
+    document.getElementById("bikinibottom").innerHTML = /*html*/ `
+        <img id="thuBotCloth" class="bikinibottom" src="../assets/images/clothes/botcloth3.png" alt="" />
+    `
+}
+
+function thuBotCloth4() {
+    document.getElementById("bikinibottom").innerHTML = /*html*/ `
+        <img id="thuBotCloth" class="bikinibottom" src="../assets/images/clothes/botcloth4.png" alt="" />
+    `
+}
+
+function thuBotCloth5() {
+    document.getElementById("bikinibottom").innerHTML = /*html*/ `
+        <img id="thuBotCloth" class="bikinibottom" src="../assets/images/clothes/botcloth5.png" alt="" />
+    `
+}
+
+// Shoes
+function thuShoes1() {
+    document.getElementById("feet").innerHTML = /*html*/ `
+        <img src="../assets/images/shoes/shoes1.png" alt="" />
+    `
+}
+
+function thuShoes2() {
+    document.getElementById("feet").innerHTML = /*html*/ `
+        <img src="../assets/images/shoes/shoes2.png" alt="" />
+    `
+}
+
+function thuShoes3() {
+    document.getElementById("feet").innerHTML = /*html*/ `
+        <img src="../assets/images/shoes/shoes3.png" alt="" />
+    `
+}
+
+function thuShoes4() {
+    document.getElementById("feet").innerHTML = /*html*/ `
+        <img src="../assets/images/shoes/shoes4.png" alt="" />
+    `
+}
+
+function thuShoes5() {
+    document.getElementById("feet").innerHTML = /*html*/ `
+        <img src="../assets/images/shoes/shoes5.png" alt="" />
+    `
+}
+
+// Handbag
+function thuHandbag1() {
+    document.getElementById("handbag").innerHTML = /*html*/ `
+        <img src="../assets/images/handbags/handbag1.png" alt="" />
+    `
+}
+
+function thuHandbag2() {
+    document.getElementById("handbag").innerHTML = /*html*/ `
+        <img src="../assets/images/handbags/handbag2.png" alt="" />
+    `
+}
+
+function thuHandbag3() {
+    document.getElementById("handbag").innerHTML = /*html*/ `
+        <img src="../assets/images/handbags/handbag3.png" alt="" />
+    `
+}
+
+// Necklace
+function thuNecklace1() {
+    document.getElementById("necklace").innerHTML = /*html*/ `
+        <img src="../assets/images/necklaces/necklace1.png" alt="" />
+    `
+}
+
+function thuNecklace2() {
+    document.getElementById("necklace").innerHTML = /*html*/ `
+        <img src="../assets/images/necklaces/necklace2.png" alt="" />
+    `
+}
+
+function thuNecklace3() {
+    document.getElementById("necklace").innerHTML = /*html*/ `
+        <img src="../assets/images/necklaces/necklace3.png" alt="" />
+    `
+}
+
+// Hairstyle
+function thuHairstyle1() {
+    document.getElementById("hairstyle").innerHTML = /*html*/ `
+        <img src="../assets/images/hairstyle/hairstyle1.png" alt="" />
+    `
+}
+
+function thuHairstyle2() {
+    document.getElementById("hairstyle").innerHTML = /*html*/ `
+        <img src="../assets/images/hairstyle/hairstyle2.png" alt="" />
+    `
+}
+
+function thuHairstyle3() {
+    document.getElementById("hairstyle").innerHTML = /*html*/ `
+        <img src="../assets/images/hairstyle/hairstyle3.png" alt="" />
+    `
+}
+
+// Background
+function thuBackground1() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth1.png" alt="" />
+    `
+}
+
+function thuBackground2() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth2.png" alt="" />
+    `
+}
+
+function thuBackground3() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth3.png" alt="" />
+    `
+}
+
+function thuBackground4() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth4.png" alt="" />
+    `
+}
+
+function thuBackground5() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth5.png" alt="" />
+    `
+}
+
+function thuBackground6() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth6.png" alt="" />
+    `
+}
+
+function thuBackground6() {
+    document.getElementById("bikinitop").innerHTML = /*html*/ `
+        <img id="thuTopCloth" class="bikinitop" src="../assets/images/clothes/topcloth6.png" alt="" />
+    `
+}
